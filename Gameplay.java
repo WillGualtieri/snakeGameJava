a2803f4b7af3d85eb8ada2485c888de98f0b7fc3
nicknamesScorpius <wgualtieri1116@gmail.com>
2019-07-02 14:56:27 -0400

    Beginning of Snake Game via: https://www.youtube.com/watch?v=_SqnzvJuKiA&list=PLz5rnvLVJX5WPzzaJucyHujFCZNhVOivR I have created the two classes Main and Gameplay. In Gameplay.java, I have learned about the JFrame and created one for the game.

---
+++ Gameplay.java
@@ -0,0 +1,16 @@
+import javax.swing.JPanel;
+
+import java.awt.Graphics;
+
+public class Gameplay extends JPanel{
+	
+		public Gameplay()
+		{
+			
+		}
+
+		public void paint(Graphics g)
+		{
+			
+		}
+}
